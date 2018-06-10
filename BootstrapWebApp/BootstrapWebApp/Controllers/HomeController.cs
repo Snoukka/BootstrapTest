6daafd94c810bb65f1572955a51b3cd0c6478084
Jyri Mattila <jyri.mattila@stud.pointcollege.fi>
2018-06-10 15:08:53 +0300

    Lisätty Bootstrapin Modal-esimerkki.

--- BootstrapWebApp/BootstrapWebApp/Controllers/HomeController.cs
+++ BootstrapWebApp/BootstrapWebApp/Controllers/HomeController.cs
@@ -26,5 +26,14 @@ namespace BootstrapWebApp.Controllers
 
             return View();
         }
+
+        public ActionResult Ruudukko()
+        {
+            return View();
+        }
+        public ActionResult Modal()
+        {
+            return View();
+        }
     }
 }
